15c2f26a9008f43af4958969632f9a84637744d5
gemperline <agemperline@csu.fullerton.edu>
2019-04-11 17:22:42 -0700

    commit

--- balance.cpp
+++ balance.cpp
@@ -7,7 +7,9 @@
 #include <fstream>
 #include <iomanip>
 
+
 using namespace std;
+
 
 
 struct PERSON
@@ -18,10 +20,8 @@ struct PERSON
 
 
 
-void deposit(char custName[20], PERSON P[],int SIZE)
+void deposit(char custName[20], PERSON P[],int SIZE, double amount)
 {
-  double deposit;
-
   for(int i = 0; i < SIZE; i++)
   {
     // strcmp() will compare c_str types
@@ -29,17 +29,8 @@ void deposit(char custName[20], PERSON P[],int SIZE)
     // NOTE: strcmp() will return 0 if TRUE, 1 if FALSE
     if (strcmp(custName, P[i].Name) == 0)
     {
-      cout << P[i].Name << ", how much would you like to deposit?" << endl;
-      cin >> deposit;
-
-      P[i].balance += deposit;
+      P[i].balance += amount;
       cout << P[i].Name << "'s new balance is: " << fixed <<setprecision(2) << P[i].balance << endl;
-
-      break;
-    }
-    else if (i == SIZE - 1)
-    {
-      cout << "ERROR: name not found." << endl;
     }
   }
 }
@@ -94,21 +85,25 @@ void newCopy(string fileName, PERSON P[], int SIZE)
 
 PERSON * readData(PERSON P[], int N)
 {
-  PERSON * tempPtr;
-
   ifstream inFile;
   inFile.open("data.txt");
 
   // traverse the array, create new person, read data, store data
   for(int i = 0; i < N; i++)
   {
-    string fName;
-    string lName;
-    string fullName;
+    char fName[9];
+    char lName[10];
 
     // read name to local members, read balance to struct, then copy name to struct
     inFile >> fName >> lName >> P[i].balance;
-    fullName = fName + " " + lName;
+
+//    int fNameSize = fName.size();
+  //  int lNameSize = lName.size();
+//    int fullNameSize = fNameSize + lNameSize;
+
+    string fullName(fName);
+
+  //  fullName = fName + lName;
     strcpy(P[i].Name, fullName.c_str());
   }
   inFile.close();
@@ -138,17 +133,16 @@ void countRecords(int &numRecords)
   }
   else
     cout << "ERROR: could not open file" << endl;
-
-  cout << "NUM RECORDS: " << numRecords << endl;
 }
 
 
 
 int main()
 {
-  string line;
+  string fname, lname, fullName;
   char custName[20];
   int numRecords = 0;
+  double depositAmt;
 
   // open data file and count records
     countRecords(numRecords);
@@ -166,10 +160,30 @@ int main()
     findRichest(P, numRecords);
 
     cout << "Enter your full name to deposit money:" << endl;
-    getline(cin, line);
-    strcpy(custName, line.c_str());
+    cin >> setw(9) >> fname;
+    cin >> setw(10) >> lname;
+    fullName = fname + " " + lname;
 
-    deposit(custName, P, numRecords);
+    // convert fullName string to custName c_string
+    strcpy(custName, fullName.c_str());
+
+    for(int i = 0; i < numRecords; i++)
+    {
+      // strcmp() will compare c_str types
+      // function compares lhs with rhs
+      // NOTE: strcmp() will return 0 if TRUE, 1 if FALSE
+      if (strcmp(custName, P[i].Name) == 0)
+      {
+        cout << custName << ", how much would you like to deposit?" << endl;
+        cin >> depositAmt;
+
+        deposit(custName, P, numRecords, depositAmt);
+      }
+      else if (i == numRecords-1)
+      {
+        cout << "ERROR: name not found." << endl;
+      }
+    }
 
     // update the input file with new balance
     newCopy("data.txt", P, numRecords);
