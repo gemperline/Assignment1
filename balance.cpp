59303572f609c291328685a1f4e6903660e4f79c
gemperline <39207952+gemperline@users.noreply.github.com>
2019-05-09 14:20:24 -0700

    Update balance.cpp

--- balance.cpp
+++ balance.cpp
@@ -44,7 +44,7 @@ void findRichest(PERSON P[],int SIZE)
 
   for(int i = 1; i < SIZE; i++)
   {
-    if (P[0].balance < P[i+1].balance)
+    if (P[0].balance < P[i].balance)
       richest = i;
   }
 
